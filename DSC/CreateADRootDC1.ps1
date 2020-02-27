6e2e4ff0cea8c08f5f931ee1d3d3cd4c65a7d94e
Dave Barnett <dbarnett@microsoft.com>
2020-02-27 09:44:53 -0500

    Update CreateADRootDC1.ps1

--- DSC/CreateADRootDC1.ps1
+++ DSC/CreateADRootDC1.ps1
@@ -4,15 +4,12 @@ configuration CreateADRootDC1
    (
         [Parameter(Mandatory)]
         [String]$DomainName,
-        
-        [Parameter(Mandatory)]
-        [String]$DnsForwarder,
 
         [Parameter(Mandatory)]
         [System.Management.Automation.PSCredential]$Admincreds
     )
 
-    Import-DscResource -ModuleName xActiveDirectory, xStorage, xNetworking, cDisk
+    Import-DscResource -ModuleName xActiveDirectory, xStorage, xNetworking
     
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
     $Interface = Get-NetAdapter | Where-Object Name -Like "Ethernet*" | Select-Object -First 1
@@ -39,62 +36,7 @@ configuration CreateADRootDC1
             DependsOn = "[WindowsFeature]DNS"
         }
         
-        Script SetDNSForwarder
-        {
-            #
-            # 
-            #
-            SetScript =
-            {
-                $dnsrunning = $false
-                $triesleft = $Using:RetryCount
-                While (-not $dnsrunning -and ($triesleft -gt 0))
-                {
-                    $triesleft--
-                    try
-                    {
-                        $dnsrunning = (Get-Service -name dns).Status -eq "running"
-                    } catch {
-                        $dnsrunning = $false
-                    }
-                    if (-not $dnsrunning)
-                    {
-                        Write-Verbose -Verbose "Waiting $($Using:RetryIntervalSec) seconds for DNS service to start"
-                        Start-Sleep -Seconds $Using:RetryIntervalSec
-                    }
-                }
-
-                if (-not $dnsrunning)
-                {
-                    Write-Warning "DNS service is not running, cannot edit forwarder. Template deployment will fail."
-                    # but continue anyway.
-                }
-                try {
-                    Write-Verbose -Verbose "Getting list of DNS forwarders"
-                    $forwarderlist = Get-DnsServerForwarder
-                    if ($forwarderlist.IPAddress)
-                    { 
-                        Write-Verbose -Verbose "Removing forwarders"
-                        Remove-DnsServerForwarder -IPAddress $forwarderlist.IPAddress -Force
-                    } else {
-                        Write-Verbose -Verbose "No forwarders found"
-                    }
-                } catch {
-                    Write-Warning -Verbose "Exception running Remove-DNSServerForwarder: $_"
-                }
-                try {
-                    Write-Verbose -Verbose "setting  forwarder to $($using:DNSForwarder)"
-                    Set-DnsServerForwarder -IPAddress $using:DNSForwarder
-                } catch {
-                    Write-Warning -Verbose "Exception running Set-DNSServerForwarder: $_"
-                }
-                 
-            }
-            GetScript =  { @{} }
-            TestScript = { $false }
-            DependsOn = "[WindowsFeature]DNSTools"
-        }
-      
+        
         WindowsFeature ADTools
         {
             Ensure = "Present"
@@ -115,26 +57,12 @@ configuration CreateADRootDC1
             Name = "RSAT-DFS-Mgmt-Con"
             DependsOn = "[WindowsFeature]DNS"
         }
-<#
-        xWaitforDisk Disk2
-        {
-            DiskID = 2
-            RetryIntervalSec =$RetryIntervalSec
-            RetryCount = $RetryCount
-        }
-        
-        cDiskNoRestart ADDataDisk
-        {
-            DiskNumber = 2
-            DriveLetter = "F"
-            DependsOn = "[xWaitForDisk]Disk2"
-        }
-  #>      
+   
         WindowsFeature ADDSInstall
         {
             Ensure = "Present"
             Name = "AD-Domain-Services"
-            #DependsOn = "[cDiskNoRestart]ADDataDisk"
+            DependsOn = "[WindowsFeature]DNS"
         }
 
         #
@@ -156,7 +84,7 @@ configuration CreateADRootDC1
             DatabasePath = "C:\NTDS"
             LogPath = "C:\NTDS"
             SysvolPath = "C:\SYSVOL"
-            DependsOn = @("[WindowsFeature]ADDSInstall", "[xDnsServerAddress]DnsServerAddress", "[Script]SetDNSForwarder")
+            DependsOn = @("[WindowsFeature]ADDSInstall", "[xDnsServerAddress]DnsServerAddress")
         }
     }
 }
