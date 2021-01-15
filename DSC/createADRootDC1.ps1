8eae559b418374295a3ef5c9172ffedbb44e82f9
Dave Barnett <dbarnett@microsoft.com>
2021-01-15 14:35:00 -0500

    Add files via upload

--- DSC/createADRootDC1.ps1
+++ DSC/createADRootDC1.ps1
@@ -11,7 +11,13 @@
         [Int]$RetryCount=20,
         [Int]$RetryIntervalSec=30
     ) 
-    Import-DscResource -ModuleName xActiveDirectory, xNetworking, PSDesiredStateConfiguration, xPendingReboot
+    Import-DscResource -ModuleName xActiveDirectory
+    Import-DscResource -ModuleName xNetworking
+    Import-DscResource -ModuleName xPendingReboot
+    Import-DscResource -ModuleName PSDesiredStateConfiguration -ModuleVersion 2.0.5
+    Import-DscResource -ModuleName xDnsServer
+    Import-DscResource -ModuleName xADRecycleBin
+     
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
     $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
     $InterfaceAlias=$($Interface.Name)
@@ -57,7 +63,7 @@
             AddressFamily  = 'IPv4'
 	        DependsOn = "[WindowsFeature]DNS"
         }
-
+        
         WindowsFeature ADDSInstall 
         { 
             Ensure = "Present" 
@@ -88,7 +94,32 @@
             LogPath = "C:\NTDS"
             SysvolPath = "C:\SYSVOL"
 	        DependsOn = @("[WindowsFeature]ADDSInstall")
-        } 
+        }
+        
+        xADRecycleBin RecycleBin
+        {
+            EnterpriseAdministratorCredential = $DomainCreds
+            ForestFQDN                        = $DomainName
+            DependsOn                         = '[xWaitForADDomain]DC1Forest'
+        }
+
+        # borrowed from https://github.com/brwilkinson/AzureDeploymentFramework/blob/main/ADF/ext-DSC/DSC-ADPrimary.ps1#L156
+        # when the DC is promoted the DNS (static server IP's) are automatically set to localhost (127.0.0.1 and ::1) by DNS
+        # I have to remove those static entries and just use the Azure Settings for DNS from DHCP
+        
+        Script ResetDNS
+        {
+            DependsOn  = '[xADRecycleBin]RecycleBin'
+            GetScript  = { @{Name = 'DNSServers'; Address = { Get-DnsClientServerAddress -InterfaceAlias Ethernet* | foreach ServerAddresses } } }
+            SetScript  = { Set-DnsClientServerAddress -InterfaceAlias Ethernet* -ResetServerAddresses -Verbose }
+            TestScript = { Get-DnsClientServerAddress -InterfaceAlias Ethernet* -AddressFamily IPV4 | 
+                    Foreach { ! ($_.ServerAddresses -contains '127.0.0.1') } }
+        }
+        xPendingReboot
+        {
+            Name = "RebootforDCPromo"
+            DependsOn = '[Script]ResetDNS'
+        }
 
    }
 } 
