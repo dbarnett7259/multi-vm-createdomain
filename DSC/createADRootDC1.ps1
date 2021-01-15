dcf48a40b24b5dff6bf1b5a1963c82dcca0797a0
Dave Barnett <dbarnett@microsoft.com>
2021-01-15 15:05:39 -0500

    Add files via upload

--- DSC/createADRootDC1.ps1
+++ DSC/createADRootDC1.ps1
@@ -11,12 +11,7 @@
         [Int]$RetryCount=20,
         [Int]$RetryIntervalSec=30
     ) 
-    Import-DscResource -ModuleName xActiveDirectory
-    Import-DscResource -ModuleName xNetworking
-    Import-DscResource -ModuleName xPendingReboot
-    Import-DscResource -ModuleName PSDesiredStateConfiguration -ModuleVersion 2.0.5
-    Import-DscResource -ModuleName xPSDesiredStateConfiguration
-    Import-DscResource -ModuleName xDnsServer
+    Import-DscResource -ModuleName xActiveDirectory, xNetworking, PSDesiredStateConfiguration, xPendingReboot
      
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
     $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
@@ -30,8 +25,7 @@
             ActionAfterReboot = 'continueConfiguration'
             RebootNodeIfNeeded = $true
         }
-
-         
+                 
 	    WindowsFeature DNS 
         { 
             Ensure = "Present" 
@@ -129,6 +123,5 @@
             Name = "RebootforDCPromo"
             DependsOn = '[Script]ResetDNS'
         }
-
    }
 } 
