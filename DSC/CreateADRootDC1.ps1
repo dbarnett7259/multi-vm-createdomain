6a5bff997ec8aac216f6444a19a763219779b22c
Dave Barnett <dbarnett@microsoft.com>
2021-02-04 14:00:46 -0500

    Update CreateADRootDC1.ps1

--- DSC/CreateADRootDC1.ps1
+++ DSC/CreateADRootDC1.ps1
@@ -35,6 +35,7 @@
             ConfigurationMode    = 'ApplyAndMonitor'
             RebootNodeIfNeeded   = $true
             AllowModuleOverWrite = $true
+            psdscallowplaintextpassword = $true
         }
 
           WindowsFeature DNS
@@ -120,20 +121,10 @@
             DependsOn                     = "[WindowsFeature]InstallADDS"
         }
 
-        xWaitForADDomain DC1Forest
-        {
-            DomainName           = $DomainName
-            DomainUserCredential = $DomainCreds
-            RetryCount           = $RetryCount
-            RetryIntervalSec     = $RetryIntervalSec
-            DependsOn            = "[xADDomain]DC1"
-        } 
-
         xADRecycleBin RecycleBin
         {
             EnterpriseAdministratorCredential = $DomainCreds
             ForestFQDN                        = $DomainName
-            DependsOn                         = '[xWaitForADDomain]DC1Forest'
         }
 
         # when the DC is promoted the DNS (static server IP's) are automatically set to localhost (127.0.0.1 and ::1) by DNS
